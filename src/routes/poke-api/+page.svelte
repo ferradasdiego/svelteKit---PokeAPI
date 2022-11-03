8eb61531d33f822a56e3bebb81dc55e93eb6c62c
ferradasdiego <32649684+ferradasdiego@users.noreply.github.com>
2022-11-03 21:52:05 +0100

    1 file modified (styles for poke-api view)

--- src/routes/poke-api/+page.svelte
+++ src/routes/poke-api/+page.svelte
@@ -6,11 +6,18 @@
 	async function getPkmByNumber(){
 		let request = await fetch ("/poke-api?pkmNumber="+inputNumberValue);
 		pokemon = await request.json();
+		fillImputs();
 	}
 
 	async function getPkmByName(){
 		let request = await fetch ("/poke-api?pkmNumber="+inputNameValue);
 		pokemon = await request.json();
+		fillImputs();
+	}
+
+	function fillImputs(){
+		inputNumberValue=pokemon.id;
+		inputNameValue=pokemon.name;
 	}
 </script>
 
@@ -19,28 +26,71 @@
 	<meta name="PokeApi" content="Consultas pokeapi" />
 </svelte:head>
 
-<div class="pokemon">
-	{#if pokemon?.sprites}
-		<img src={pokemon?.sprites?.front_default} alt="">
-		<img src={pokemon?.sprites?.back_default} alt="">
-		<p>Id: {pokemon.id}</p>
-		<p>Name: {pokemon.name}</p>
-		<p>Height: {pokemon.height}</p>
-		<p>Weight: {pokemon.weight}</p>
+<div id="container">
+{#if pokemon?.sprites}
+	<div id="pokemon">
+		<div id="pokemonImgs">
+			<img class="pkmImg" src={pokemon?.sprites?.front_default} alt="">
+			<img class="pkmImg" src={pokemon?.sprites?.back_default} alt="">
+		</div>
+		<div id="pokemonInfo">
+			<p class="infoLabel">Id: {pokemon.id}</p>
+			<p class="infoLabel">Name: {pokemon.name}</p>
+			<p class="infoLabel">Height: {pokemon.height}</p>
+			<p class="infoLabel">Weight: {pokemon.weight}</p>			
+		</div>
+	</div>
 	{/if}
+
+	<div id="inputsContainer">
+		<div>
+			<input type="number" bind:value={inputNumberValue}>
+			<button on:click={getPkmByNumber}>Get pokemon by number</button>
+		</div>
+
+		<div>
+			<input type="text" bind:value={inputNameValue}>
+			<button on:click={getPkmByName}>Get pokemon by name</button>
+		</div>
+	</div>
 </div>
 
-<div>
-	<input type="number" bind:value={inputNumberValue}>
-	<button on:click={getPkmByNumber}>Get pokemon by number</button>
-
-	<input type="text" bind:value={inputNameValue}>
-	<button on:click={getPkmByName}>Get pokemon by name</button>
-</div>
 
 <style>
-	.pokemon{
-		text-align:center;
+	#container{
+		display: flex;
+    justify-content: space-around;
+    align-items: center;
+	}
+
+	#pokemon{
+		background-color:red;
+		border: 1em solid red;
+	}
+
+	.pkmImg{
+		border: 1px solid red;
+	}
+
+	#inputsContainer{
+
+	}
+
+	#pokemonImgs{
+		background-color:white;
+	}
+
+	#pokemonInfo{
+
+	}
+
+	#pokemonImgs{
+		display:flex;
+	}
+
+	.infoLabel{
+		background-color:white;
+		padding: 5px;
 	}
 </style>
 
